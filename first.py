1073c7c155b2b4ca9ad288c313f2b22a98f22575
vishalb999 <vishalsb339@gmail.com>
2026-09-11 09:16:52 +0530

    h

--- first.py
+++ first.py
@@ -1,3 +1,30 @@
-for i in range(5):
-    print(i)
-print(i)
+words = [
+    "hello",
+    "vishal",
+    "good",
+    "morning",
+    "how",
+    "are",
+    "you",
+    "python",
+    "java",
+    "coding",
+    "programming",
+    "developer",
+    "student",
+    "computer",
+    "software",
+    "engineer",
+    "learning",
+    "practice",
+    "success",
+    "career"
+]
+words.sort()
+sor={}
+for word in range(len(words)):
+    if(len(words[word])>5):
+        sor[words[word]]=len(words[word])
+
+print(sor)
+        
